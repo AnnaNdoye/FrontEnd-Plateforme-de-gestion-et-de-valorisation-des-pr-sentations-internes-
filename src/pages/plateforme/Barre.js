bef3128ccde936fc372c48e1f5218d4c1c2d4ac3
AnnaNdoye <ndoyeanna754@gmail.com>
2025-10-16 09:11:54 +0000

    connexion réussie modification profil réussie----- problème en cours de résolution3

--- src/pages/plateforme/Barre.js
+++ src/pages/plateforme/Barre.js
@@ -24,7 +24,7 @@ const Sidebar = styled.div`
     color: white;
     padding: 2rem 1.5rem;
     box-shadow: 5px 0 25px rgba(0,0,0,0.15);
-    height: 100vh;
+    height: 1000px;
     overflow-y: auto;
     position: relative;
     z-index: 10;
